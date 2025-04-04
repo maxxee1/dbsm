313f137eead523e1db2bcef2cec1f9b50dac9309
maxxee1 <maximiliano.adonis@gmail.com>
2025-04-04 01:02:21 -0300

    .

--- Datos_Cargar/setChecks.py
+++ Datos_Cargar/setChecks.py
@@ -8,19 +8,19 @@ def procesar_datos(input_file='C:/Code/dbsm/Datos_Cargar/masivoo.sql', output_fi
         # Preparamos el encabezado del archivo SQL de salida
         outfile.write("INSERT INTO persona1 (RUT, Nombre, Edad, Direccion) VALUES\n")
 
-        # Procesar cada fila del archivo CSV
         for row in reader:
             RUT = row[0].strip()  
             Nombre = row[1].strip()  
             Edad = row[2].strip()  
             Direccion = row[3].strip()  
 
-            # Asegurarse de que el RUT tenga 10 dígitos, agregando ceros al final si es menor
             if len(RUT) < 10:
-                RUT = RUT.ljust(10, '0')  # Añadir ceros al final del RUT hasta que tenga 10 dígitos
+                RUT = RUT.ljust(10, '0')  
+            
+            if len(Edad) == 1:
+                Edad = 'X' + Edad 
 
-            # Formatear cada fila en el formato SQL (con comillas simples para cadenas)
-            insert_statement = f"({RUT},'{Nombre}',{Edad},'{Direccion}'),\n"
+            insert_statement = f"('{RUT}','{Nombre}','{Edad}','{Direccion}'),\n"
             outfile.write(insert_statement)
 
 # Llamar a la función
